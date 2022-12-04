69858b0ebc3e88a55e3173ed0de6d431d006d464
Ollie Jones <oathompsonjones@gmail.com>
2022-12-04 17:44:40 +0000

    Fix previous commit

--- Rules/TypeScriptReact.js
+++ Rules/TypeScriptReact.js
@@ -1,6 +1,6 @@
 const { ERROR } = require("./Constants");
 
-let supportedTypeScriptEslintRules = { "naming-convention": [ERROR, { format: ["camelCase"], leadingUnderscore: "allow", selector: "default", trailingUnderscore: "allow" }, { format: ["camelCase", "UPPER_CASE"], leadingUnderscore: "allow", selector: "variable", trailingUnderscore: "allow" }, { format: ["PascalCase"], selector: "typeLike" }, { format: ["camelCase", "PascalCase"], selector: "function" }] };
+let supportedTypeScriptEslintRules = { "naming-convention": [ERROR, { format: ["camelCase"], leadingUnderscore: "allow", selector: "default", trailingUnderscore: "allow" }, { format: ["camelCase", "UPPER_CASE", "PascalCase"], leadingUnderscore: "allow", selector: "variableLike", trailingUnderscore: "allow" }, { format: ["PascalCase"], selector: "typeLike" }, { format: ["camelCase", "PascalCase"], selector: "function" }] };
 supportedTypeScriptEslintRules = Object.fromEntries(Object.entries(supportedTypeScriptEslintRules).map(([key, value]) => [`@typescript-eslint/${key}`, value]));
 
 module.exports = { ...supportedTypeScriptEslintRules };
